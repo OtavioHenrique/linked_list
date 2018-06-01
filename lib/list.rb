84014a5426e526e4a1665dda4c5e4aca68f377ea
Otavio Henrique <otaviopvaladares@gmail.com>
2018-06-01 01:34:26 -0300

    List#add method, shift and pop

--- lib/list.rb
+++ lib/list.rb
@@ -1,9 +1,40 @@
 # frozen_string_literal: true
 
 class List
-  attr_accessor :head
+  attr_accessor :head, :tail
 
-  def initialize(head: nil)
+  def initialize(head: nil, tail: nil)
     @head = head
+    @tail = tail
+  end
+
+  def add(value)
+    new_node = Node.new(value: value)
+
+    self.head = new_node if head.nil?
+    self.tail = new_node if tail.nil?
+
+    new_node.previus_node = self.tail
+    self.tail.next_node = new_node
+
+    self.tail = new_node
+  end
+
+  def pop
+    old_tail = self.tail
+
+    self.tail = old_tail.previus_node
+    self.tail.next_node = nil
+
+    old_tail.previus_node = nil
+  end
+
+  def shift
+    old_head = self.head
+
+    self.head = old_head.next_node
+    self.head.previus_node = nil
+
+    old_head.next_node = nil
   end
 end
--- test/list_test.rb
+++ test/list_test.rb
@@ -10,5 +10,71 @@ class ListTest < Minitest::Test
 
     assert_equal list.head, node
   end
+
+  def test_list_tail
+    node = Node.new(value: 123)
+    list = List.new(tail: node)
+
+    assert_equal list.tail, node
+  end
+
+  def test_list_tail_setter
+    node = Node.new(value: 123)
+    list = List.new
+    list.tail = node
+
+    assert_equal list.tail, node
+  end
+
+  def test_list_head_setter
+    node = Node.new(value: 123)
+    list = List.new
+    list.head = node
+
+    assert_equal list.head, node
+  end
+
+  def test_add_node_on_empty_list
+    list = List.new
+    list.add(12)
+
+    assert_equal list.head.value, 12
+  end
+
+  def test_add_node
+    list = List.new
+
+    for number in 1..10 do
+      list.add(number)
+    end
+
+    list.add(99)
+
+    assert_equal list.tail.value, 99
+  end
+
+  def test_list_pop
+    list = List.new
+
+    for number in 1..10 do
+      list.add(number)
+    end
+
+    list.pop
+
+    assert_equal list.tail.value, 9
+  end
+
+  def test_list_shift
+    list = List.new
+
+    for number in 1..10 do
+      list.add(number)
+    end
+
+    list.shift
+
+    assert_equal list.head.value, 2
+  end
 end
 
